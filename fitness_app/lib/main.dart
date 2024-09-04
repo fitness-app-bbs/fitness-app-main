efef819fbd0a3935001378237efde2ca0d0fbc9a
Lorenz <marvin.rocket.64@gmail.com>
2024-09-04 13:22:18 +0200

    fixed error switching pages, added first impl. of homepage

--- fitness_app/lib/main.dart
+++ fitness_app/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'homepage.dart';
 
 void main() {
   runApp(MyApp());
@@ -45,12 +46,10 @@ class MyAppState extends ChangeNotifier {
   }
 }
 
-
 class MyHomePage extends StatefulWidget {
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
-
 
 class _MyHomePageState extends State<MyHomePage> {
   var selectedIndex = 0;
@@ -60,59 +59,57 @@ class _MyHomePageState extends State<MyHomePage> {
     Widget page;
     switch (selectedIndex) {
       case 0:
-        page = Placeholder();
+        page = HomePage(); // Here, HomePage is assigned correctly
         break;
       case 1:
-        page = Placeholder();
+        page = Placeholder(); // You can replace Placeholder with another widget
         break;
       case 2:
-        page = Placeholder();
+        page = Placeholder(); // You can replace Placeholder with another widget
         break;
       case 3:
-        page = Placeholder();
+        page = Placeholder(); // You can replace Placeholder with another widget
         break;
       default:
         throw UnimplementedError('no widget for $selectedIndex');
     }
 
-    return LayoutBuilder(builder: (context, constraints) {
-      return Scaffold(
-        bottomNavigationBar: NavigationBar(
-          selectedIndex: selectedIndex,
-          onDestinationSelected: (int index) {
-            setState(() {
-              selectedIndex = index;
-            });
-          },
-          destinations: const <Widget>[
-            NavigationDestination(
-              selectedIcon: Icon(Icons.home),
-              icon: Icon(Icons.home_outlined),
-              label: 'Home',
-            ),
-            NavigationDestination(
-              selectedIcon: Icon(Icons.sports),
-              icon: Icon(Icons.sports_outlined),
-              label: 'Workouts',
-            ),
-            NavigationDestination(
-              selectedIcon: Icon(Icons.fastfood_rounded),
-              icon: Icon(Icons.fastfood_rounded),
-              label: 'Nutrition',
-            ),
-            NavigationDestination(
-              selectedIcon: Icon(Icons.leaderboard),
-              icon: Icon(Icons.leaderboard_outlined),
-              label: 'Leaderboard',
-            ),
-          ],
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return Scaffold(
+          bottomNavigationBar: NavigationBar(
+            selectedIndex: selectedIndex,
+            onDestinationSelected: (int index) {
+              setState(() {
+                selectedIndex = index;
+              });
+            },
+            destinations: const <Widget>[
+              NavigationDestination(
+                selectedIcon: Icon(Icons.home),
+                icon: Icon(Icons.home_outlined),
+                label: 'Home',
+              ),
+              NavigationDestination(
+                selectedIcon: Icon(Icons.sports),
+                icon: Icon(Icons.sports_outlined),
+                label: 'Workouts',
+              ),
+              NavigationDestination(
+                selectedIcon: Icon(Icons.fastfood_rounded),
+                icon: Icon(Icons.fastfood_rounded),
+                label: 'Nutrition',
+              ),
+              NavigationDestination(
+                selectedIcon: Icon(Icons.leaderboard),
+                icon: Icon(Icons.leaderboard_outlined),
+                label: 'Leaderboard',
+              ),
+            ],
           ),
-        ),
-      );
-    });
+          body: page, // Display the selected page here
+        );
+      },
+    );
   }
 }
