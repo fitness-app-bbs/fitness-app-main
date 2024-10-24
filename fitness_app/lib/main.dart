105ff798918397ac8e11e4019793c1a4b162f8d5
niklasprg <niklas.puehringer@icloud.com>
2024-10-24 14:24:02 +0200

    fixed reloading problems and shows navbar everytime also when page opend about tiles

--- fitness_app/lib/main.dart
+++ fitness_app/lib/main.dart
@@ -9,9 +9,11 @@ import 'utils/colors.dart';
 import 'pages/leaderboard.dart';
 import 'pages/workouts.dart';
 import 'pages/nutrition.dart';
+import 'pages/calorie_calculator_page.dart';
+import 'pages/meal_recipe_page.dart';
+import 'pages/settings.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
-
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -73,79 +75,84 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   var selectedIndex = 0;
 
+  final List<Widget> _pages = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _pages.addAll([
+      HomePage(onTileTap: (index) {
+        setState(() {
+          selectedIndex = index;
+        });
+      }),
+      WorkoutPage(),
+      NutritionDashboard(),
+      LeaderboardPage(),
+      SettingsPage(),
+      CalorieCalculatorPage(),
+    ]);
+    void _openSettingsPage() {
+      Navigator.of(context).push(
+        MaterialPageRoute(builder: (context) => SettingsPage()),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    Widget page;
-    switch (selectedIndex) {
-      case 0:
-        page = HomePage();
-        break;
-      case 1:
-        page = WorkoutPage();
-        break;
-      case 2:
-        page = NutritionDashboard();
-        break;
-      case 3:
-        page = LeaderboardPage();
-        break;
-      default:
-        throw UnimplementedError('no widget for $selectedIndex');
-    }
-
-    return LayoutBuilder(
-      builder: (context, constraints) {
-        return Scaffold(
-          backgroundColor: AppColors.white,
-          bottomNavigationBar: NavigationBarTheme(
-            data: CustomNavBarTheme.theme,  // Anwenden des Farbschemas
-            child: NavigationBar(
-              selectedIndex: selectedIndex,
-              onDestinationSelected: (int index) {
-                setState(() {
-                  selectedIndex = index;
-                });
-              },
-              destinations: const <Widget>[
-                NavigationDestination(
-                  selectedIcon: Icon(Icons.home),
-                  icon: Icon(Icons.home_outlined),
-                  label: 'Home',
-                ),
-                NavigationDestination(
-                  selectedIcon: Icon(Icons.sports),
-                  icon: Icon(Icons.sports_outlined),
-                  label: 'Workouts',
-                ),
-                NavigationDestination(
-                  selectedIcon: Icon(Icons.fastfood_rounded),
-                  icon: Icon(Icons.fastfood_rounded),
-                  label: 'Nutrition',
-                ),
-                NavigationDestination(
-                  selectedIcon: Icon(Icons.leaderboard),
-                  icon: Icon(Icons.leaderboard_outlined),
-                  label: 'Leaderboard',
-                ),
-              ],
+    return Scaffold(
+      backgroundColor: AppColors.white,
+      body: IndexedStack(
+        index: selectedIndex,
+        children: _pages,
+      ),
+      bottomNavigationBar: NavigationBarTheme(
+        data: CustomNavBarTheme.theme,
+        child: NavigationBar(
+          selectedIndex: selectedIndex,
+          onDestinationSelected: (int index) {
+            setState(() {
+              selectedIndex = index;
+            });
+          },
+          destinations: const <Widget>[
+            NavigationDestination(
+              selectedIcon: Icon(Icons.home),
+              icon: Icon(Icons.home_outlined),
+              label: 'Home',
             ),
-          ),
-          body: page,
-        );
-      },
+            NavigationDestination(
+              selectedIcon: Icon(Icons.sports_rounded),
+              icon: Icon(Icons.sports_outlined),
+              label: 'Workouts',
+            ),
+            NavigationDestination(
+              selectedIcon: Icon(Icons.fastfood_rounded),
+              icon: Icon(Icons.fastfood_outlined),
+              label: 'Nutrition',
+            ),
+            NavigationDestination(
+              selectedIcon: Icon(Icons.leaderboard),
+              icon: Icon(Icons.leaderboard_outlined),
+              label: 'Leaderboard',
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
 
-  class CustomNavBarTheme {
-    static NavigationBarThemeData get theme {
-      return NavigationBarThemeData(
+class CustomNavBarTheme {
+  static NavigationBarThemeData get theme {
+    return NavigationBarThemeData(
       backgroundColor: AppColors.whitepink,
       indicatorColor: AppColors.lightpink,
       labelTextStyle: MaterialStateProperty.all(TextStyle(
         color: AppColors.black,
         fontWeight: FontWeight.bold,
-        )),
+      )),
       iconTheme: MaterialStateProperty.resolveWith<IconThemeData>((Set<MaterialState> states) {
         if (states.contains(MaterialState.selected)) {
           return IconThemeData(
@@ -154,9 +161,8 @@ class _MyHomePageState extends State<MyHomePage> {
         }
         return IconThemeData(
           color: AppColors.black,
-          );
-        }),
-      );
-    }
+        );
+      }),
+    );
   }
-
+}
